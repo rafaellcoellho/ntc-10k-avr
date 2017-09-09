31f0e6ec8afb974cc3f639ddd15f0dee31ef6175
Rafael Coelho <rafaellcoellho@gmail.com>
2017-09-09 19:49:18 -0300

    Update AD.h

--- src/AD.h
+++ src/AD.h
@@ -1,4 +1,13 @@
 /*
+* ----------------------------------------------------------------------------
+* "THE BEER-WARE LICENSE" (Revision 42):
+* <rafaellcoellho@gmail.com> wrote this file. As long as you retain this notice you
+* can do whatever you want with this stuff. If we meet some day, and you think
+* this stuff is worth it, you can buy me a beer in return Poul-Henning Kamp
+* ----------------------------------------------------------------------------
+*/
+
+/*
 	@file						AD.h
 	@autor					Rafael Coelho <rafaellcoellho@gmail.com>
 	@brief					Header with functions for AD interaction.
